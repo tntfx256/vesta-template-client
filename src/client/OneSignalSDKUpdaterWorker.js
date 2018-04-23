f78cf0259187a144e64366a2674a40abfbc4194a
hbtb <bishehsary@gmail.com>
2018-04-23 13:12:36 +0430

    domain

--- src/client/OneSignalSDKUpdaterWorker.js
+++ src/client/OneSignalSDKUpdaterWorker.js
@@ -1,4 +1,4 @@
 var VERSION = '__TIMESTAMP__';
 
-importScripts('https://app.autoapp.ir/service-worker.js?v=__TIMESTAMP__');
+importScripts('https://app.vesta.bz/service-worker.js?v=__TIMESTAMP__');
 importScripts('https://cdn.onesignal.com/sdks/OneSignalSDK.js');
